d11fec40e8d59d9703a076af53bb0fc3d5cff62a
Jordan Patel <logicaljordan247@gmail.com>
2022-07-19 19:35:03 +0200

    controllers edited

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @bookmarks = @list.bookmarks
   end
 
   def new
@@ -14,9 +15,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
-
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   private
