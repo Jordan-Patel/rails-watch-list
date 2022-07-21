2fe84222c21b8119e5a5be16788bdf12b9a4202e
Jordan Patel <logicaljordan247@gmail.com>
2022-07-21 20:59:46 +0200

    styling updates

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   # The comment of a bookmark cannot be shorter than 6 characters.
   validates :comment, length: { minimum: 6 }
-  validates_uniqueness_of :movie_id, scope: [:list_id]
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
+  resources :bookmarks, only: :destroy
 end
